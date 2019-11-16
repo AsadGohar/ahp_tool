f664560c59312649506b1d897478b04d62295b5d
Rumman Sadiq <chrumman@gmail.com>
2019-11-17 01:05:10 +0500

    Update RequirementsModal.jsx

--- src/Components/RequirementsModal.jsx
+++ src/Components/RequirementsModal.jsx
@@ -1,56 +1,62 @@
-import React, { Component } from 'react';
-import { Modal, Tabs, Form } from 'antd';
-import RequirementsForm from './RequirementsForm';
-import Matrix from './Matrix';
-import { stat } from 'fs';
-import NewMatrix from './NewMatrix';
+import React, { Component } from "react";
+import { Modal, Tabs, Form } from "antd";
+import RequirementsForm from "./RequirementsForm";
+import Matrix from "./Matrix";
+import { stat } from "fs";
+import NewMatrix from "./NewMatrix";
 
 const { TabPane } = Tabs;
-const WrappedForm = Form.create({ name: 'dynamic_form_item' })(RequirementsForm);
+const WrappedForm = Form.create({ name: "dynamic_form_item" })(
+  RequirementsForm
+);
 
 export default class RequirementsModal extends Component {
-    constructor(props) {
-        super(props);
-    }
-    state = {
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    requirements: 0,
+    dataSource: [],
+    columns: []
+  };
 
-        requirements:0,
-        dataSource:[],
-        columns:[]
-    };
+  setData = x => {
+    this.setState({
+      dataSource: x
+    });
+  };
 
-    setData = (x) => {
-        this.setState({
-            dataSource:x
-        });
-    }
+  setColumn = x => {
+    this.setState({
+      columns: x
+    });
+  };
 
-    
-    setColumn = (x) => {
-        this.setState({
-            columns:x
-        });
-    }
-
-    render() {
-        const {visible, handleCancel, handleOk, activeTab, okText } = this.props;
-        const{requirements}= this.state;
-        return (
-            <Modal
-                visible={visible}
-                onOk={handleOk}
-                onCancel={handleCancel}
-                okText={okText}
-                >
-                <Tabs activeKey={activeTab.toString()}>
-                    <TabPane tab="Requirements" key="1">
-                        <WrappedForm dataSource = {this.state.setData} columns = {this.state.setColumn}/>
-                    </TabPane>
-                    <TabPane tab="Relative Intensity" key="2">
-                        <NewMatrix dataSource = {this.state.dataSource} columns = {this.state.columns} / >
-                    </TabPane>
-                </Tabs>
-            </Modal>
-        );
-    }
+  render() {
+    const { visible, handleCancel, handleOk, activeTab, okText } = this.props;
+    const { requirements } = this.state;
+    return (
+      <Modal
+        visible={visible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        okText={okText}
+      >
+        <Tabs activeKey={activeTab.toString()}>
+          <TabPane tab="Requirements" key="1">
+            <WrappedForm
+              dataSource={this.state.setData}
+              columns={this.state.setColumn}
+            />
+          </TabPane>
+          <TabPane tab="Relative Intensity" key="2">
+            <NewMatrix
+              dataSource={this.state.dataSource}
+              columns={this.state.columns}
+            />
+          </TabPane>
+        </Tabs>
+      </Modal>
+    );
+  }
 }
